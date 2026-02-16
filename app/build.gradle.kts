937e9bdb33b8e31176dfa573576db2f034dff698
Iaroslav Belozerov <iarbelozerov@gmail.com>
2026-02-17 00:37:41 +0100

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "tech.tarakoshka.bridgemich"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.0.1"
+        versionCode = 2
+        versionName = "0.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
